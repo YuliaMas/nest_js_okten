c9437da7c7cd686eea92ad922d05054d80c083ec
Yuliya Masechko <yulia.masechko@gmail.com>
2024-10-16 18:09:52 +0300

    add api properties for users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'John Doe', description: 'The name of the User' })
+  readonly name: string;
+  @ApiProperty({
+    example: 30,
+    description: 'The age of the User',
+    minimum: 0,
+    maximum: 120,
+  })
+  readonly age: number;
+  @ApiProperty({
+    example: 'john_doe@test.com',
+    description: 'The email of the User',
+    required: false,
+  })
+  readonly email: string;
+  @ApiProperty({ example: 'JohnDoe1', description: 'The password of the User' })
+  readonly password?: string;
+  @ApiProperty()
+  readonly role: string;
+}

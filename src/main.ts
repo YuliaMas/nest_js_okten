391bcc518e4a74640a02a9868025b2ff472d79f9
Yuliya Masechko <yulia.masechko@gmail.com>
2024-10-23 00:53:14 +0300

    config

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { SwaggerHelper } from './common/helpers/swagger.helper';
+import { AppConfig } from './configs/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('NestJS 2024 OktenWeb')
-    .setDescription('The cats API description')
+    .setTitle('NestJS-2024 OktenWeb')
+    .setDescription('The users API description')
     .setVersion('1.0')
     .addTag('users')
-    .addBasicAuth({
+    .addBearerAuth({
       in: 'header',
       type: 'http',
       scheme: 'bearer',
@@ -41,12 +43,19 @@ async function bootstrap() {
     },
   });
 
-  const port = 3000;
-  const host = 'localhost';
+  // const port = 3000;
+  // const host = 'localhost';
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get<AppConfig>('app');
 
   await app.listen(3000, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-    console.log(`Server is running on http://${host}:${port}/api`);
+    // console.log(configService.get('app'));
+    console.log(
+      `Server is running on http://${appConfig.host}:${appConfig.port}`,
+    );
+    console.log(
+      `Swagger is running on http://${appConfig.host}:${appConfig.port}/api`,
+    );
   });
 }
 void bootstrap();
